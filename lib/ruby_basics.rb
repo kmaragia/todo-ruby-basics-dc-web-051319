0dc5a9b460048936830ce9638aa51b99bd8592f6
Kemunto Maragia <kmaragia@email.wm.edu>
2020-03-26 18:13:58 +0000

    Automatically backed up by Learn

---
+++ lib/ruby_basics.rb
@@ -0,0 +1,26 @@
+def division (num1,num2)
+  return num1/num2
+end
+
+def assign_variable (name)
+end
+
+def argue(arg)
+  return arg
+end
+
+def greeting (arg1,arg2)
+  
+end
+
+def return_a_value
+  return "Nice"
+end
+
+def last_evaluated_value
+  return "expert"
+end 
+
+def pizza_party(name = "cheese")
+  return name 
+end
